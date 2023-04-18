979f1ea4cf89680a1df94ed45503acce67e8350d
asep3003 <saepulloha133@gmail.com>
2023-04-19 03:06:09 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ["*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'comf': ['Comfortaa', 'sans-serif'],
+        'lora': ['Lora', 'serif'],
+        'nun': ['Nunito', 'sans-serif'],
+        'libre': ['Libre Baskerville', 'serif'],
+        'monte': ["Montserrat", "sans-serif"],
+        sans: ['Nunito', ... defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+};
